5827db9f0130ada258aea6699167e145668a5360
Julie <juliezeng8@@gmail.com>
2022-08-17 07:34:10 -0700

    pushing up to GitHub added print statement

---
+++ finalProject1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  finalProject1
+//
+//  Created by Julie  on 8/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("this is my final proj")
+    }
+
+}
+
