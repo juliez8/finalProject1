93a5eac5775c3e5f51c310ed7d03c8e3771cc39e
Julie <juliezeng8@@gmail.com>
2022-08-19 02:45:06 -0700

    fact gen and resource page, ready for quiz and to do list before class tomorrow

--- EigthViewController.swift
+++ EigthViewController.swift
@@ -14,8 +14,25 @@ class EigthViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var factGen: UITextView!
     
-
+    @IBAction func factGenButton(_ sender: Any) {
+        
+        let array = [
+        
+        "Libyan food is a mix of Arab, Mediterranean, Italian, & African food!",
+        "The hottest temperature ever recorded officially on Earth was in 1923 Al-Azizia, Libya, which was 136.4 degrees!",
+        "Language: There are 121 languages and 270 mother tongues in India",
+        "A famous dish made in Tamil Nadu is idli (savory cakes made from fermented rice) and sambar (lentil soup)",
+        "The Taj Mahal is one of the seven wonders of the world, and the king who commissioned it for his wife forced all the workers to cut their hands off so it could never be made again",
+        "China has 56 official ethnic groups",
+        "Wheat-based dishes such as buns, breads, and wheat-based noodles are the staple food item in Northern China. Rice is the staple food item in Southern China",
+        "Israelis favor a direct communication style, conversations are often passionate, loud, and expressive",
+        "in Israel, from sundown on Friday to sundown on Saturday, many businesses shut down to observe shabbat (includes restaurants, public transportation, shops)",
+        ]
+        factGen.text = array.randomElement()
+    }
+    
     /*
     // MARK: - Navigation
 
